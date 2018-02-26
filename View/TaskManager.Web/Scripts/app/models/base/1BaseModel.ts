63be24d27684e4c13a16fe52d51a776c69534bf3
Pilipenko Anton <pilipenko.anton@gmail.com>
2018-02-26 10:09:02 +0700

    hides a edit link in older comments shows a add button as link

--- View/TaskManager.Web/Scripts/app/models/base/1BaseModel.ts
+++ View/TaskManager.Web/Scripts/app/models/base/1BaseModel.ts
@@ -5,12 +5,17 @@
         public CreatedById: string;
         public CreatedDate: moment.Moment;
 
+        // hides a edit link in older comments
+        public CheckByAge(): boolean {
+            return moment().diff(this.CreatedDate, "hours") < 12;
+        }
+
         constructor(data: any) {
             super();
             if (data != null) {
                 this.EntityId = data.EntityId;
                 this.CreatedById = data.CreatedById;
-                this.CreatedDate = moment(data.CreatedDate);
+                this.CreatedDate = moment.utc(data.CreatedDate);
             } else {
                 this.CreatedDate = moment();
             }
